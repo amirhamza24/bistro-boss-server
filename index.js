1d0622496a08e9b6e2c0d0386892fe22c17362fd
amirhamza24 <ridoykhan7897890@gmail.com>
2023-06-12 01:19:19 +0600

    payment method added

--- index.js
+++ index.js
@@ -173,14 +173,16 @@ async function run() {
     })
 
     // create payment intent
-    app.post('/create_payment_intent', verifyJWT, async (req, res) => {
+    app.post('/create-payment-intent', verifyJWT, async (req, res) => {
         const { price } = req.body;
         const amount = price * 100;
-        const paymentIntent = await stripe.paymentIntent.create({
+        console.log(price, amount);
+        const paymentIntent = await stripe.paymentIntents.create({
             amount: amount,
             currency: 'usd',
             payment_method_types: ['card']
         });
+
         res.send({
             clientSecret: paymentIntent.client_secret
         })
