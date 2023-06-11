34065fd1fedf9bfb58a59348315eab1ae9d19e90
amirhamza24 <ridoykhan7897890@gmail.com>
2023-06-11 18:34:25 +0600

    payment method added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -171,6 +172,19 @@ async function run() {
         res.send(result);
     })
 
+    // create payment intent
+    app.post('/create_payment_intent', async (req, res) => {
+        const { price } = req.body;
+        const amount = price * 100;
+        const paymentIntent = await stripe.paymentIntent.create({
+            amount: amount,
+            currency: 'usd',
+            payment_method_types: ['card']
+        });
+        res.send({
+            clientSecret: paymentIntent.client_secret
+        })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
